9b965771d1e241e707be366dd54f71124f5de4bd
lambmikkel <66314737+lambmikkel@users.noreply.github.com>
2020-07-29 10:17:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var shopper = {
+  fullname: "",
+  store_member: false,
+  cart_price: 0,
+  grocery_cart:[],
+  add_cart: function(item) {
+    return this.grocery_cart.push(item)
+  }
+};
+
+  var new_shopper = {
+    
+  }
+  
+  
