3e6033c6acee8cd42b7d1d0fe86ec189b444715b
lambmikkel <66314737+lambmikkel@users.noreply.github.com>
2020-08-16 21:08:03 -0700

    Final draft

--- week_3/functions/functions.js
+++ week_3/functions/functions.js
@@ -2,15 +2,15 @@
 function sum(num1, num2) {
   return num1 + num2;
 }
-var result = sum(6,8);
+var result = sum(6, 8);
 console.log(result);
 
 //Three numbers returns largest
-var numbers = [8,10,12,13,24];
+var numbers = [8, 10, 12, 13, 24];
 var largestNumber = (values) => {
   var highest = 0;
-  for(var i=0; i<values.length; i+=1){
-    if(values[i] > highest){
+  for (var i = 0; i < values.length; i += 1) {
+    if (values[i] > highest) {
       highest = values[i];
     }
   }
@@ -26,8 +26,31 @@ function isEven(number) {
 var result1 = isEven(8);
 console.log(result1);
 
-//
-var stringOne = "My name is Mikkel";
-var stringTwo = "Lamb and I'm 31 years old";
-var result2 = stringOne <= stringTwo;
-console.log(result);
+
+
+const str = "Mozilla is the best site for me";
+
+function concatString(str) {
+  if (str.length > 20) {
+    // Get the length of the string
+    let stringLength = str.length;
+
+    console.log("Length of string =", stringLength);
+
+    // Get half of the string length, divide by 2 to get half!
+    let halfString = stringLength / 2;
+
+    console.log("Half of string =", halfString);
+
+    // Slice start at first index, ends with halfString
+    console.log(str.substring(0, halfString));
+    str = str.substring(0, halfString);
+  } else {
+    //
+    str = str.concat(str);
+    console.log(str);
+  }
+  return str;
+}
+
+concatString(str);
